a653638eea7612535f0630a9c2ed0c7f39fc53e6
Jaden ACHAIN <jaden.achain@gmail.com>
2024-04-15 12:08:21 +0200

    Add manifests

--- index.php
+++ index.php
@@ -2,9 +2,10 @@
 <!DOCTYPE html>
 <html lang="en" xmlns="http://www.w3.org/1999/html">
 <head>
-	<title>Server status</title>
+	<title>Server status 1.0.1</title>
 	<meta content="text/html" charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="manifest.webmanifest" rel="manifest">
 
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 	<style>
